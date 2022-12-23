763b40eb566242e425a7acced6e6840c2f12afea
TirapatS <tirapat.s02@gmail.com>
2022-12-23 14:47:06 +0100

    installed and setup firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Firebase Auth instance 
+const auth = getAuth(app)
+
+// Firestore instance
+const db = getFirestore(app)
+
+// Storgare instance
+const storage = getStorage(app)
+
+export {
+    app as default,
+	auth,
+    db,
+    storage,
+}
