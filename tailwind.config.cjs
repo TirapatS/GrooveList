df7801f21f6c4f57e1dbef3e4b3fb1364250b1d8
TirapatS <tirapat.s02@gmail.com>
2022-12-28 13:53:57 +0100

    added flowbite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,26 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
+    colors: {
+      'GLblack': '#232323',
+      'GLwhite': '#f9f9f9'
+    },
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
