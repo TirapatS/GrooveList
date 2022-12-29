fbaa28a733f356db9dafd8755e894d2238d8ac32
TirapatS <tirapat.s02@gmail.com>
2022-12-29 13:57:02 +0100

    forgot username input

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -14,12 +14,18 @@ const Signup = () => {
                                 Create an account
                             </h1>
                             <form class="space-y-4 md:space-y-6" action="#">
+
                                 <div>
-                                    <label for="email" class="block mb-2 text-sm font-bodymedium text-gray-900 dark:text-white">Your email</label>
+                                    <label for="username" class="block mb-2 text-sm font-body text-GLwhite">Your username</label>
+                                    <input type="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Groover" required/>
+                                </div>
+
+                                <div>
+                                    <label for="email" class="block mb-2 text-sm font-body text-GLwhite">Your email</label>
                                     <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com" required/>
                                 </div>
                                 <div>
-                                    <label for="password" class="block mb-2 text-sm font-body text-gray-900 dark:text-white">Password</label>
+                                    <label for="password" class="block mb-2 text-sm font-body text-GLwhite">Password</label>
                                     <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
                                 </div>
                                
@@ -31,7 +37,7 @@ const Signup = () => {
                         </div>
                     </div>
                 </div>
-                </section>
+            </section>
         </>
     )
 }
