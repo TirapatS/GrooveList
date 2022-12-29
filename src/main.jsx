260dcf258a4f35aecef51a19ad1ad1128bf503f7
TirapatS <tirapat.s02@gmail.com>
2022-12-29 15:38:48 +0100

    imported AuthContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from './App'
 import './index.css'
 import 'flowbite';
 import { BrowserRouter } from 'react-router-dom';
+import AuthContextProvider from './contexts/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-			<App />
+      <AuthContextProvider>
+        <App/>
+      </AuthContextProvider>
 		</BrowserRouter>
   </React.StrictMode>,
 )
