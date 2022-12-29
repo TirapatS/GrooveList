da63280f3ba7cf3b8eb6314a986bb2f3f1626dcc
TirapatS <tirapat.s02@gmail.com>
2022-12-29 14:47:26 +0100

    installed react-toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import HomePage from './pages/HomePage';
 import ExplorePage from './pages/ExplorePage';
@@ -7,19 +7,37 @@ import SignupPage from './pages/SignupPage';
 import RecentlyPlayedPage from './pages/RecentlyPlayedPage';
 import './assets/css/generic.css'
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage/>}/>
-      <Route path="/home" element={<HomePage/>}/>
-      <Route path="/explore" element={<ExplorePage/>}/>
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path="signup" element={<SignupPage/>}/>
-      <Route path="recently-played" element={<RecentlyPlayedPage/>}/>
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/home" element={<HomePage/>}/>
+        <Route path="/explore" element={<ExplorePage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="signup" element={<SignupPage/>}/>
+        <Route path="recently-played" element={<RecentlyPlayedPage/>}/>
 
-      {/*TODO Favourites and Create Album page is only available for logged in users */}
-    </Routes>
+        {/*TODO Favourites and Create Album page is only available for logged in users */}
+      </Routes>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </>
   )
 }
 
