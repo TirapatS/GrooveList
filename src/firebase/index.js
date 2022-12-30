e9428be264bb9c7f3d21d79f0f77d1a5473a693d
TirapatS <tirapat.s02@gmail.com>
2022-12-30 15:01:24 +0100

    now import correctly from env file

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -8,10 +8,10 @@ import { getStorage } from 'firebase/storage'
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -23,12 +23,8 @@ const auth = getAuth(app)
 // Firestore instance
 const db = getFirestore(app)
 
-// Storgare instance
-const storage = getStorage(app)
-
 export {
     app as default,
 	  auth,
-    db,
-    storage,
+    db
 }
