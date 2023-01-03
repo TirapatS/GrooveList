b74b859c8f0174d28ad55e3f41ea9a31fd4c51d2
TirapatS <tirapat.s02@gmail.com>
2023-01-03 14:02:46 +0100

    Import navs

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,51 @@
+import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+import { deviceWidthState } from '../atoms/global.js'
+import LargeDeviceNav from "../components/LargeDeviceNav";
+import SearchBar from "../components/SearchBar.jsx";
+import SmallDeviceNav from "../components/SmallDeviceNav"
 
 const HomePage = () => {
+  const [smallDevice, setSmallDevice] = useState(null)
+  const [largeDevice, setLargeDevice] = useState(null)
+  const width = useRecoilValue(deviceWidthState)
+  
+  useEffect(()=> {
+
+    if(width < 1024) {
+      setSmallDevice(width)
+    }else {
+      setLargeDevice(width)
+    }
+  }, [])
+
+  
+  
   return (
-    <div>HomePage</div>
+    <>
+      { smallDevice && (
+        <>
+          <div>
+            <SmallDeviceNav/>
+            <SearchBar/>
+          </div>
+        </>
+
+      )}
+
+      { largeDevice && (
+        <>
+          <div className="flex">
+            <LargeDeviceNav/>
+            <div className="ml-10">
+              <SearchBar/>
+            </div>
+          </div>
+        </>
+      )}
+
+      
+    </>
   )
 }
 
