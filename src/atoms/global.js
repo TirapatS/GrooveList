22c01cfc70997e424702325b3b5a1590b6e3da8c
TirapatS <tirapat.s02@gmail.com>
2023-01-08 14:14:06 +0100

    store selectedCateogry inside of an atom to later read it in other components

--- src/atoms/global.js
+++ src/atoms/global.js
@@ -11,3 +11,8 @@ export const selectedTrackState = atom({
     key: 'selectedTrack',
     default: []
 })
+
+export const selectedCategoryState = atom({
+    key: 'selectedCategory',
+    default: ''
+})
