b2a8dea0b584cd04ed4b0a96f8cf94c0f4e31f32
TirapatS <tirapat.s02@gmail.com>
2023-01-09 12:03:32 +0100

    added Page Counter

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -15,6 +15,7 @@ const ExplorePage = () => {
     const width = useRecoilValue(deviceWidthState)
     const [categories, setCategories] = useState(null)
     const [page, setPage] = useState(0)
+    const [clicks, setClicks] = useState(1)
     const [next, setNext] = useState(null)
     const [prev, setPrev] = useState(null)
     const navigate = useNavigate()
@@ -39,6 +40,16 @@ const ExplorePage = () => {
         setCategoryState(category)
         navigate(`/category/${category}`)
     }
+
+    const handlePrev = (num) => {
+        setPage(prevValue => prevValue - 20)
+        setClicks(prevValue => prevValue - 1)
+    }
+
+    const handleNext = (num) => {
+        setPage(prevValue => prevValue + 20)
+        setClicks(prevValue => prevValue + 1)
+    }
       
     useEffect(()=> {
     if(width < 1024) {
@@ -49,6 +60,7 @@ const ExplorePage = () => {
     }, [])
 
     useEffect(() => {
+
         fetchData(page)
     }, [page])
     
@@ -70,17 +82,20 @@ const ExplorePage = () => {
                                     <CardList data={categories} handleClick={handleClick}/> 
                                     <div className="mb-20">   
                                         <div className="flex flex-col items-center">
+                    
+                                            <span className="font-semibold text-GLwhite">Page {clicks}</span>
+                                    
                                             <div className="inline-flex mt-2 xs:mt-0">
                                                 {
                                                     (prev) ? 
-                                                    <button onClick={() => setPage(prevValue => prevValue - 20)} className="inline-flex items-center px-4 py-2 text-sm font-body text-white bg-gray-600 rounded-l bg-opacity-25">
+                                                    <button onClick={() => handlePrev(20)} className="inline-flex items-center px-4 py-2 text-sm font-body text-white bg-gray-600 rounded-l bg-opacity-25">
                                                         <svg aria-hidden="true" className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd"></path></svg>
                                                     Prev
                                                     </button> : null
                                                 }
                                                 {
                                                     (next) ?
-                                                    <button onClick={() => setPage(prevValue => prevValue + 20)} className="inline-flex items-center px-4 py-2 text-sm font-body text-white bg-gray-600 border-0 border-l border-gray-700 bg-opacity-25 rounded-r ">
+                                                    <button onClick={() => handleNext(20)} className="inline-flex items-center px-4 py-2 text-sm font-body text-white bg-gray-600 border-0 border-l border-gray-700 bg-opacity-25 rounded-r ">
                                                         Next
                                                         <svg aria-hidden="true" className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                                                     </button>
