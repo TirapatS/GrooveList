3177e7a84d477c1aeb5638d3e35d031878839c26
TirapatS <tirapat.s02@gmail.com>
2023-01-09 10:45:14 +0100

    added more margin for space

--- src/components/ShowcasePlaylist.jsx
+++ src/components/ShowcasePlaylist.jsx
@@ -3,7 +3,7 @@ const ShowcasePlaylist = ({ data, handleClick }) => {
     console.log(data)
     return (
         <>
-            <div className="bg-GLblack rounded-lg">
+            <div className="bg-GLblack rounded-lg mb-[100px]">
                 <div className="grid laptop:grid-rows-4 laptop:grid-flow-col gap-4 grid-rows-6 grid-flow-col p-2 laptop:p-0">
                     {data.items.map((item) => {
                         return (
