f421158f38a5756896b2a39d92079ebdb7f1fdd7
TirapatS <tirapat.s02@gmail.com>
2023-01-10 10:37:05 +0100

    fix childrens with the same key warning

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,13 +1,13 @@
 
 const CardList = ({ data, handleClick }) => {    
-    
+    console.log(data)
     return (
         <>
-            <div className="bg-GLblack rounded-lg">
+            <div className="bg-GLblack rounded-lg mb-[100px]">
                 <div className="grid laptop:grid-rows-4 laptop:grid-flow-col gap-4 grid-rows-6 grid-flow-col p-2 laptop:p-0">
-                    {data.items.map((item) => {
+                    {data.items.map((item, idx) => {
                         return (
-                            <div onClick={() => handleClick(item.id)} className="my-3 laptop:w-[120px] w-[69px]" key={item.id}>
+                            <div onClick={() => handleClick(item.id)} className="my-3 laptop:w-[120px] w-[69px]" key={idx}>
                                 {
                                     (item.icons?.[0].url) ? <img className="rounded-lg hover:p-1" src={item.icons?.[0].url} title={item.name}/> 
                                     :
