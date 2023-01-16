2af9f3c3962aae62eed8f0006834f92a64835f0b
TirapatS <tirapat.s02@gmail.com>
2023-01-16 14:23:03 +0100

    check if clicked track is liked, if true unlike it otherwise like it, send back to backend

--- src/components/ScrollList.jsx
+++ src/components/ScrollList.jsx
@@ -1,13 +1,13 @@
 import { useAuthContext } from "../contexts/AuthContext"
 import useFavourites from "../hooks/useFavourites"
-import { SubmitFavourites } from '../utils/handleFavourites'
+import { handleFavourites } from '../utils/handleFavourites'
 import { isThisTrackLiked } from "../utils/isThisTrackLiked"
 
 
 const ScrollList = ({ data }) => {
     const { currentUser } = useAuthContext()
     const [ docs, loading, error ] = useFavourites(currentUser)
-
+    console.log(docs)
 
     return (
         <div className="relative my-5 bg-gray-600 bg-opacity-25 rounded-xl">
@@ -40,7 +40,7 @@ const ScrollList = ({ data }) => {
                                 </div>
 
                            
-                                <div className="mt-2" onClick={(e) => SubmitFavourites(item, currentUser, e)}>
+                                <div className="mt-2" onClick={(e) => handleFavourites(item, currentUser, docs, e)}>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill={isThisTrackLiked(docs, item.id) ? "red" : "gray"} className="w-8 h-8 laptop:w-12 h-12">
                                         <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
                                     </svg>
--- src/utils/handleFavourites.js
+++ src/utils/handleFavourites.js
@@ -1,17 +1,30 @@
-import { doc, setDoc } from "firebase/firestore"
+import { deleteDoc, doc, setDoc } from "firebase/firestore"
 import { toast } from "react-toastify"
 import { db } from "../firebase"
+import { isThisTrackLiked } from "./isThisTrackLiked"
 
-export const SubmitFavourites = async (item, currentUser, e) => {
+export const handleFavourites = async (item, currentUser, docs, e) => {
     e.preventDefault()
+
 
     if(!currentUser) {
         toast.error('You must be logged in to use this feature')
         return
     }
     
-    const docRef = doc(db, `users/${currentUser.email}/favourites`, item.id)
-    await setDoc(docRef, {
-        item
-    })
+    const liked = isThisTrackLiked(docs, item.id)
+    console.log(liked)
+
+    if(!liked) {
+        const docRef = doc(db, `users/${currentUser.email}/favourites`, item.id)
+            await setDoc(docRef, {
+            item
+        })
+    } else {
+        const docRef = doc(db, `users/${currentUser.email}/favourites`, item.id)
+            await deleteDoc(docRef, {
+            item
+        })
+    }
+    
 }
