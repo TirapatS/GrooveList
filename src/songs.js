fa61efaa2563b0c21af3a5c9cfa18397716c21fc
TirapatS <tirapat.s02@gmail.com>
2023-01-20 23:06:37 +0100

    fixed bug and added redirects for netlify

--- src/songs.js
+++ src/songs.js
@@ -1,5 +1,5 @@
 import id1Img from '../src/assets/images/after_dark.jpg'
-import id2Img from '../src/assets/images/Hate_It_or_love_It.jpg'
+import id2Img from '../src/assets/images/Hate_It_or_Love_It.jpg'
 import id3Img from '../src/assets/images/piece_of_your_heart_meduza.jpg'
 import afterDark from '../src/assets/mp3/Mr_Kitty_After_Dark.mp3'
 import hateItOrLoveIt from '../src/assets/mp3/Hate_It_Or_Love_It_The_Game.mp3'
